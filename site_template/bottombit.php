7a1a7da189b52e9bd55758eb95b67cebd6335cfd
chabati-creator <61482164+chabati-creator@users.noreply.github.com>
2020-08-29 23:41:53 +1200

    Initial commit

---
+++ site_template/bottombit.php
@@ -0,0 +1,15 @@
+        <div class="box side">
+           
+           
+            
+        </div> <!-- / side bar -->
+        
+        <div class="box footer">
+            CC GTT 20XX
+        </div> <!-- / footer -->
+                
+        
+    </div> <!-- / wrapper -->
+    
+            
+</body>
